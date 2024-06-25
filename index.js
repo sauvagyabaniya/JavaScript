a8b27e84c95f95e5807398dea837ea48d9445782
Sauvagya <sauvagyabaniya@gmail.com>
2024-06-25 09:17:57 +0545

    Adding

---
+++ index.js
@@ -0,0 +1,73 @@
+// console.log("Here is js");
+// alert("Click on ok to run");
+// // Variables        (var)
+// var x=60;
+// console.log(x);
+// var x=600;
+// console.log(x);
+
+
+// // let
+// let y=4555;
+// console.log(y);
+// // we cannot write let to declare same value but if we write y nad remove let it wont show error
+// y=4555;
+// console.log(y);
+
+// // const
+// const result =90;
+// console.log(result);
+// // we cannot declare same variable
+// // after removing const we cannot run code 
+// // can only declare one time
+// // const result =90;
+// // console.log(result);
+// // result =90;
+// // console.log(result);
+
+// // DATA TYPES
+// // /string
+// const output="Rainbow";
+// console.log(output);
+// console.log(typeof output);
+// // type of is used to see the data type
+
+// // number
+// // const aa="65656";
+// // yo halyo vani string dekhauxa"""
+// const aa=65656;
+// // const paxi j halda ne hunxa
+// console.log(aa);
+// console.log(typeof aa);
+
+// // float
+// const x=65.565;
+// console.log(x);
+// console.log(typeof x);
+
+
+// // undefined
+// let g;
+// console.log(g);
+// console.log(typeof g);
+// // const wont define undefined 
+
+// null
+let f = null;
+console.log(f);
+console.log(typeof f);
+
+// Boolean
+const isTrue = true;
+console.log(isTrue);
+console.log(typeof isTrue);
+
+// BigInt
+const i = true;
+console.log(i);
+console.log(typeof i);
+
+// Symbol
+const s= Symbol("Rainbow is my good boy");
+console.log(s);
+console.log(typeof s);
